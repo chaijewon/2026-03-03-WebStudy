2c871bbcb59f564e347b7752ca9fbf3a6b289db7
chaijewon <vcandjava@nate.com>
2026-06-29 17:03:58 +0900

    1111

--- JSPVueProject_3/src/main/java/com/sist/model/MemberModel.java
+++ JSPVueProject_3/src/main/java/com/sist/model/MemberModel.java
@@ -1,5 +1,39 @@
 package com.sist.model;
 
-public class MemberModel {
+import com.sist.commons.Commons;
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+import com.sist.vo.*;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import com.sist.dao.*;
+@Controller
+public class MemberModel {
+   @RequestMapping("member/login.do")
+   public void member_login(HttpServletRequest request,
+		   HttpServletResponse response)
+   {
+	   String id=request.getParameter("id");
+	   String pwd=request.getParameter("pwd");
+	   
+	   MemberVO vo=MemberDAO.isLogin(id, pwd);
+	   if(vo.getMsg().equals("OK"))
+	   {
+		   HttpSession session=request.getSession();
+		   session.setAttribute("id", vo.getId());
+		   session.setAttribute("name", vo.getName());
+		   session.setAttribute("isAdmin", vo.getIsadmin());
+	   }
+	   
+	   Commons.sendData(response, "text/html", vo.getMsg());
+   }
+   @RequestMapping("member/logout.do")
+   public void member_logout(HttpServletRequest request,
+		   HttpServletResponse response)
+   {
+	   
+   }
 }
