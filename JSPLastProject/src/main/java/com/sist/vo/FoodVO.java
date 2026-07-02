168011f8f364b18179221ce30e8fb7d8ab855143
chaijewon <vcandjava@nate.com>
2026-07-02 12:07:39 +0900

    1111

---
+++ JSPLastProject/src/main/java/com/sist/vo/FoodVO.java
@@ -0,0 +1,27 @@
+package com.sist.vo;
+/*
+ *    패키지 
+ *     com.sist.commons  : 전체 클래스 사용하는 공통 기반 
+ *                         => DAO : SqlSessionFactory
+ *                         => Footer에서 사용 
+ *     com.sist.dao : 데이터베이스 연동 
+ *     com.sist.model : 요청 처리 결과 전송 (브라우저)
+ *     com.sist.mapper : 요청에 해당하는 SQL문장 작성 
+ *     com.sist.vo  : 데이터를 모아서 한번에 전송 
+ *     com.sist.manager : 실시간 채팅 
+ *                        이메일 전송 
+ *                        Open Api => 뉴스 / 인기검색어 
+ *     ------------------------------------------------
+ *     View 
+ *       => Jquery / Ajax / JavaScript 
+ *       => 폴더를 만들어서 처리 
+ *     ------------------------------------------------
+ *     vo / mapper (SQL) / dao (연결) / model (브라우저 전송) / JSP 출력 
+ *     ------------------------------------------------------------
+ *     
+ *     main : 공통 작업 
+ *            메뉴별 => 역할 분담 
+ */
+public class FoodVO {
+
+}
